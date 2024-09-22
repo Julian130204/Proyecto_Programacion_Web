a8b011e5038e8e99de0c92f3a5a95e4a967c5c39
FexrVP <vazquez.diego.3007@gmail.com>
2024-09-21 18:57:03 -0600

    Delete Prototipo directory

--- Prototipo/dashboard/buttons.php
+++
@@ -1,9 +0,0 @@
-<?php require_once "vistas/parte_superior.php"?>
-
-<!--INICIO del cont principal-->
-<div class="container">
-    <h1>Página de buttons</h1>
-</div>
-<!--FIN del cont principal-->
-
-<?php require_once "vistas/parte_inferior.php"?>
